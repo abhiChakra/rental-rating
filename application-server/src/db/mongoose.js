ee96ba7b5ae1021f9bc4f0869f89551f33a0bb2c
Abhishek Chakraborty <abhishek.chakraborty@mail.utoronto.ca>
2020-05-15 05:11:26 -0400

    master: modified Mongoose MongoDB client to use useUnifiedTopology

--- application-server/src/db/mongoose.js
+++ application-server/src/db/mongoose.js
@@ -3,7 +3,8 @@ require('dotenv').config()
 
 mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
-    useCreateIndex: true
+    useCreateIndex: true,
+    useUnifiedTopology: true
 })
 
 module.exports = mongoose
